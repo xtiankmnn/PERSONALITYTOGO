2ecd71463322f8901b5367d4084a5d887bbbf0de
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-05-20 22:37:40 +0200

    added model of Personality and Booking and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_202803) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_203532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "personality_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "personalities", force: :cascade do |t|
     t.string "name"
