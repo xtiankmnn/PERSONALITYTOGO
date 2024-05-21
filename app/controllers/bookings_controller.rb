5272821d8ac202d422c8bd4b84a37c9dea66bc7e
sachini weragodavidana <sachinisrimali@gmail.com>
2024-05-21 14:26:38 +0200

    change to personalities controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,50 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user # Ensure this line is based on your authentication setup
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Booking was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_url, notice: 'Booking was successfully destroyed.'
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:personality_id, :start_date, :end_date)
+  end
+end
--- app/controllers/personalities_controller.rb
+++ app/controllers/personalities_controller.rb
@@ -1,5 +1,28 @@
 class PersonalitiesController < ApplicationController
   def index
+    @personalities = Personality.all
+  end
 
+  def show
+    @personality = Personality.find(params[:id])
+  end
+
+  def new
+    @personality = Personality.new
+  end
+
+  def create
+    @personality = Personality.new(personality_params)
+    if @personality.save
+      redirect_to @personality
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def personality_params
+    params.require(:personality).permit(:name, :description, :price)
   end
 end
