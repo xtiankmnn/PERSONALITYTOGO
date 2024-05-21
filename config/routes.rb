511e191541e0e8e2ddcf805aff84d8acddf184a8
sachini weragodavidana <sachinisrimali@gmail.com>
2024-05-21 11:40:36 +0200

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users
+  resources :personalities do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, except: [:new, :create]
+
+# #get 'signup', to: 'users#new'
+ # post 'signup', to: 'users#create'
+  #get 'login', to: 'sessions#new'
+ # post 'login', to: 'sessions#create'
+ #delete 'logout', to: 'sessions#destroy
+
 end
