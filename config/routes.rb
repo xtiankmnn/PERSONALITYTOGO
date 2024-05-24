2beda78c62c5221442dd44eccade03869fd91434
Sonia Chaboud <sonia.chaboud@gmail.com>
2024-05-24 12:02:18 +0200

    flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  get "personalities/assign", to: "personalities#assign"
+  get "assign", to: "personalities#assign"
   get "personalities/:id/sell", to: "personalities#sell", as: "sell_personality"
   resources :personalities do
     resources :bookings, only: [:new, :create]
